08b843af51fb95983f26037da70fdc4f53460784
Jung Lee <113197001+junglee0035@users.noreply.github.com>
2024-09-18 15:43:31 +0000

    Added name and section #

--- BinaryTree.java
+++ BinaryTree.java
@@ -1,6 +1,6 @@
 
 /*
- * *** PLACE YOUR NAME / SECTION HERE ***
+ * *** Jung Lee / 001 ***
  *
  * Homework # 2 (Programming Assignment). This Java class defines a few basic
  * manipulation operations of a binary trees.
